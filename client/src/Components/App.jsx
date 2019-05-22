651c1f6c0bf9a9943ed11fca5c69423080bf264c
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-05-21 21:31:36 -0700

    Condition added for logo when there is click on newsletter signup

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -6,23 +6,30 @@ import DropDownMen from './DropDownMen.jsx';
 import DropDownWomen from './DropDownWomen.jsx';
 import DropDownKid from './DropDownKid.jsx';
 import DropDownSports from './DropDownSports.jsx'
+import NewsletterSignUp from './NewsletterSignUp.jsx';
 
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      view: ""
-      
+      view: "",
+      signUp: false,
     }
     this.changeView = this.changeView.bind(this);
-
+    this.handleSignUp = this.handleSignUp.bind(this);
   }
   changeView(option){
     this.setState({
       view: option
     },console.log(this.state.view))
   }
+  handleSignUp(){
+    this.setState({
+      signUp: !this.state.signUp
+    },console.log("clicked"))
+  }
+
   rendering(){
     const { view } = this.state;
     if(view === "men"){
@@ -44,14 +51,20 @@ class App extends Component {
     return (
       <div className={style.header}>
         <div>
-          <Top />
+          <Top handleSignUp={this.handleSignUp}/>
         </div>
+        {this.state.signUp ? <NewsletterSignUp /> : ""}
         <div className={style.main}>
           <div className={style.inner}>
-            <a className={style.logo}>
+          {this.state.signUp ? <a className={style.active}>
             <span className={style.only}>adidas</span>
-            </a>
-            <NavigationName changeView={this.changeView}/>
+            </a> : <a className={style.logo}>
+            <span className={style.only}>adidas</span>
+            </a>}
+            {/* <a className={style.logo}>
+            <span className={style.only}>adidas</span>
+            </a> */}
+            <NavigationName changeView={this.changeView} />
             <div>Search</div>
             <div>Shopping Cart</div>
           </div>
