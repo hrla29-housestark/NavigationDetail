959fe128310a56bf6ff90c09b2a793492d5353ad
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-05-24 12:33:55 -0700

    Able to send get request to server Able to search products

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -9,17 +9,37 @@ import DropDownSports from './DropDownSports.jsx'
 import NewsletterSignUp from './NewsletterSignUp.jsx';
 import DropDownBrands from './DropDownBrands.jsx';
 import Search from './Search.jsx';
+import DropDownSearch from './DropDownSearch.jsx'
+import axios from 'axios'
 
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      products:[],
       view: "",
       signUp: false,
+      q:''
     }
     this.changeView = this.changeView.bind(this);
     this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.fetchProducts = this.fetchProducts.bind(this);
+  }
+  componentDidMount(){
+    this.fetchProducts();
+  }
+  fetchProducts(){
+    axios
+      .get('/api/nav')
+      .then(data => {
+        console.log(data.data);
+        this.setState({
+          products: data.data
+        })
+      })
+      .catch(err => console.log('Error getting data',err))
   }
   changeView(option){
     this.setState({
@@ -31,9 +51,30 @@ class App extends Component {
       signUp: !this.state.signUp
     },console.log("clicked"))
   }
+  handleSearchChange(option){
+    this.setState({
+      q: option
+    },console.log(this.state.q))
+  }
+  renderSearch(){
+    const { view , q } = this.state;
+    var filterProducts = this.state.products.filter(product => {
+      return product.category.toLowerCase().indexOf(q) !== -1;
+    });
+    const filterCategory = [];
+    filterProducts.forEach(product => {
+      filterCategory.push({category: product.category, productNumber: product.productNumber})
+    });
+    if(q.length > 2 && view ==='' && filterProducts.length > 0){
+      return <DropDownSearch products={filterProducts.slice(0,4)} categories={filterCategory} q={this.state.q}/>
+    }else {
+      return;
+    }
+  }
 
   rendering(){
     const { view } = this.state;
+    
     if(view === "men"){
       return <DropDownMen changeView={this.changeView}/>;
     }else if(view === "women"){
@@ -68,11 +109,13 @@ class App extends Component {
             </a>}
 
             <NavigationName changeView={this.changeView} />
-            <Search />
+            <Search handleSearchChange={this.handleSearchChange}/>
             <div>Cart</div>
           </div>
-        </div>
-        {this.rendering()}
+        </div>  
+          {this.rendering()}
+          {this.renderSearch()}
+          
       </div>
 
     )
@@ -80,4 +123,17 @@ class App extends Component {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
 
